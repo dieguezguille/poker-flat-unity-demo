cc0a5c02c863dba12400c762de5b8ce96fe0054e
dieguezguille <guillermodieguez@outlook.com>
2020-06-11 01:04:54 -0300

    More fixes

--- Poker-Flat/Assets/Game/Scripts/Controllers/BackFaceController.cs
+++ Poker-Flat/Assets/Game/Scripts/Controllers/BackFaceController.cs
@@ -12,6 +12,11 @@ public class BackFaceController : MonoBehaviour
 		Globals.OnDeckTextureChanged -= OnDeckTextureChanged;
 	}
 
+	private void Start()
+	{
+		GetComponent<MeshRenderer>().material.SetTexture("_MainTex", Globals.DeckTexture);
+	}
+
 	private void OnDeckTextureChanged(object sender, Texture2D texture)
 	{
 		GetComponent<MeshRenderer>().material.SetTexture("_MainTex", texture);
